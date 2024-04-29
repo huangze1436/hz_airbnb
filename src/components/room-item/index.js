c8fae0a12567998e8f925d7a8423e3ce27d0b949
hzz <huangze@winning.com.cn>
2024-04-29 18:39:29 +0800

    20240429#页面中间房源组件实现

--- src/components/room-item/index.js
+++ src/components/room-item/index.js
@@ -5,8 +5,22 @@ import {ItemWapper} from './style'
 const RoomItem = memo((props) => {
     const {itemData} = props;
   return (
-    <ItemWapper> 
-     {itemData.name}
+    <ItemWapper verifyColor={itemData?.verify_info?.text_color || '#39576a'}> 
+      <div className='inner'>
+        <div className='cover'>
+          <img src={itemData.picture_url} alt="" />
+        </div>
+        <div className='desc' >
+          {itemData.verify_info.messages.join('.')}
+        </div>
+        <div className='name' title={itemData.name}>
+          {itemData.name}
+        </div>
+        <div className='price'>
+          ¥{itemData.price}
+        </div>
+
+      </div>
     </ItemWapper>
     
   )
--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -4,4 +4,50 @@ export const ItemWapper = styled.div`
   width: 25%;
   padding: 8px;
   box-sizing: border-box;
+  flex-shrink: 0;
+  margin: 8px 0;
+
+  .inner {
+    width: 100%;
+  }
+  .cover {
+    position: relative;
+    box-sizing: border-box;
+    padding: 66.66% 8px 0;
+    border-radius: 3px;
+    overflow: hidden;
+
+     img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+     }
+  }
+  .desc {
+    margin: 10px 0 5px;
+    font-size: 12px;
+    font-weight: 700;
+    color: ${ props => props.verifyColor};
+   
+  }
+  .name { 
+    font-size: 16px;
+    font-weight: 700;
+    height: 35px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    /* 将对象作为弹性伸缩盒子模型显示 */
+    display: -webkit-box;
+    /* 限制在一个块元素显示的文本的行数 */
+    /* -webkit-line-clamp 其实是一个不规范属性，使用了WebKit的CSS扩展属性，该方法适用于WebKit浏览器及移动端；*/
+    -webkit-line-clamp: 2;
+    /* 设置或检索伸缩盒对象的子元素的排列方式 */
+    -webkit-box-orient: vertical;
+  }
+  .price {
+    margin: 8px 0;
+  }
 `
